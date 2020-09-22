af4501a60e1dc15566ddae8230aa05238e3910d5
BOBOWang <bbw1996@qq.com>
2020-09-22 21:03:49 +0800

    验证 iconPosition 为 left 或 right 中的一个

--- src/button.vue
+++ src/button.vue
@@ -11,7 +11,16 @@
 
 <script>
   export default {
-    props: ['icon','iconPosition']
+    props: {
+      icon: {},
+      iconPosition: {
+        type: String,
+        default: 'left',
+        validator(value) {
+          return !(value !== 'left' && value !== 'right');
+        }
+      }
+    }
   }
 </script>
 
@@ -40,11 +49,26 @@
     &:focus {
       outline: none;
     }
-    > .content{order:2;}
-    > .icon {order:1;margin-right: .1em;}
-    &.icon-right{
-      > .content{order:1;}
-      > .icon{order:2;margin-right: 0;margin-left: .1em;}
+
+    > .content {
+      order: 2;
+    }
+
+    > .icon {
+      order: 1;
+      margin-right: .1em;
+    }
+
+    &.icon-right {
+      > .content {
+        order: 1;
+      }
+
+      > .icon {
+        order: 2;
+        margin-right: 0;
+        margin-left: .1em;
+      }
     }
   }
 </style>
