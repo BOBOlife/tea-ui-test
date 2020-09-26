8f003e259db39cbc876c65f1f8442727bb0e709c
BOBOWang <bbw1996@qq.com>
2020-09-26 16:36:43 +0800

    测试 让 input 支持双向绑定

--- test/input.test.js
+++ test/input.test.js
@@ -67,15 +67,18 @@ describe('Input', () => {
     it('支持 change/input/focus/blur 事件', function () {
       ['change', 'input', 'focus', 'blur']
         .forEach(eventName => {
-        vm = new Constructor({}).$mount()
-        const callback = sinon.fake()
-        vm.$on(eventName, callback)
-        //触发input的change/input/focus/blur事件
-        let event = new Event(eventName)
-        let inputElement = vm.$el.querySelector('input')
-        inputElement.dispatchEvent(event)
-        expect(callback).to.have.been.calledWith(event)
-      })
+          vm = new Constructor({}).$mount()
+          const callback = sinon.fake()
+          vm.$on(eventName, callback)
+          //触发input的change/input/focus/blur事件
+          let event = new Event(eventName)
+          Object.defineProperty(event, 'target', {
+            value: {value: 'hi'}, enumerable: true
+          })
+          let inputElement = vm.$el.querySelector('input')
+          inputElement.dispatchEvent(event)
+          expect(callback).to.have.been.calledWith('hi')
+        })
     })
   })
 })
