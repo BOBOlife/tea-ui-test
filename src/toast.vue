01da5075f7be99038dfd8f7e70f9f2ef340c68f4
BOBOWang <bbw1996@qq.com>
2020-09-30 23:11:34 +0800

    解决 currentToast 的一个bug

--- src/toast.vue
+++ src/toast.vue
@@ -71,6 +71,7 @@
       },
       close() {
         this.$el.remove()
+        this.$emit('close')
         this.$destroy()
       }
 
@@ -82,7 +83,13 @@
   $font-size: 14px;
   $toast-min-height: 40px;
   $toast-bg: rgba(0, 0, 0, 0.75);
+  @keyframes fade-in {
+    0% {opacity: 0;transform: translateY(100%)}
+    100% {opacity: 1;transform: translateY(0%)}
+  }
+
   .toast {
+    animation: fade-in 1s;
     font-size: $font-size;line-height: 1.8;min-height: $toast-min-height;
     position: fixed;display: flex;
     color: white;align-items: center;background: $toast-bg;border-radius: 4px;
